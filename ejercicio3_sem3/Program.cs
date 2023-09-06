ef02fc3376e63e36cb29ba585096cc4a6170d791
josue <josue@DESKTOP-1SO03Q0>
2023-09-05 22:09:40 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio3_sem3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio3_sem3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double premio,monto_extra, monto_adivinanza,premio_total;
+            int edad, adivinanzas_acertadas;
+            Console.WriteLine("Ingrese su edad:");
+            edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad de adivinanzas que acertó:");
+            adivinanzas_acertadas = int.Parse(Console.ReadLine());
+            if (edad >= 18)
+            {
+                if (adivinanzas_acertadas >= 3)
+                {
+                    premio = 50;
+                }
+                else
+                {
+                    premio = 30;
+                }
+            }
+            else
+            {
+                if (adivinanzas_acertadas >= 3)
+                {
+                    premio = 80;
+                }
+                else
+                {
+                    premio = 50;
+                }
+            }
+            monto_extra = edad*5;
+            monto_adivinanza = adivinanzas_acertadas*2;
+            premio_total = premio + monto_extra + monto_adivinanza;
+            Console.WriteLine("Usted gana un premio de S/." + premio_total);
+            Console.ReadKey();
+        }
+    }
+}
